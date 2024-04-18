1ca3fb8e2d72ca4d5e1a87f24a87bb5131f7ceb7
Syrus Akbary <me@syrusakbary.com>
2024-04-18 13:40:33 +0200

    Improve installation even further

--- nuitka/utils/wasi_sdk.py
+++ nuitka/utils/wasi_sdk.py
@@ -34,13 +34,21 @@ def download_sdk(before=None):
     else:
         raise Exception(f"py2wasm: Platform {platform.system()} not supported")
 
+
     url += file
+
     if not os.path.exists(os.path.join(sdk_dir, file)):
+        file_in_progress = os.path.join(sdk_dir, file + ".download")
         print(f"py2wasm: Downloading {file}")
+        try:
+            os.remove(file_in_progress)
+        except OSError:
+            pass
+
         with requests.get(url, stream=True) as req:
             req.raise_for_status()
 
-            with open(os.path.join(sdk_dir, file), "wb+") as fp, tqdm.tqdm(
+            with open(file_in_progress, "wb+") as fp, tqdm.tqdm(
                 desc=file,
                 total=int(req.headers.get("content-length", 0)),
                 unit="MiB",
@@ -49,6 +57,9 @@ def download_sdk(before=None):
             ) as progress:
                 for chunk in req.iter_content(4096):
                     progress.update(fp.write(chunk))
+
+        os.rename(file_in_progress, os.path.join(sdk_dir, file))
+
 
     print(f"py2wasm: Extracting {file}")
     with tarfile.open(os.path.join(sdk_dir, file)) as tar:
