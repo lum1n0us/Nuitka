c7a864d39fca29ae09802ac7c8ad6f698a8d714d
liang.he@intel.com <liang.he@intel.com>
2024-07-23 12:23:58 +0000

    Fix few issues after syncing up with upstream/develop

--- setup.py
+++ setup.py
@@ -435,7 +435,7 @@ setup(
         # "Programming Language :: Python :: 3.9",
         # "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
+        # "Programming Language :: Python :: 3.12",
         # We depend on CPython.
         "Programming Language :: Python :: Implementation :: CPython",
         # We generate C intermediate code and implement part of the
